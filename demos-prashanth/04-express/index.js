0996c7f5787673ecd29e1efd97a7dd7a52a726b6
Prashanth Puranik <puranik@digdeeper.in>
2021-07-10 00:10:34 +0530

    middleware

--- demos-prashanth/04-express/index.js
+++ demos-prashanth/04-express/index.js
@@ -5,9 +5,38 @@ const path = require( 'path' );
 const app = express();
 
 app.set( 'view engine', 'ejs' );
-app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'views', path.join( process.cwd(), 'views' ) );
 
+// logger middleware
+app.use( ( req, res, next ) => {
+    res.locals.receivedDate = new Date();
+    next();
+    console.log( 'response is being sent 1' );
+});
+
+app.use( function dateLogger( req, res, next ) {
+    console.log( 'New request received at time ', res.locals.receivedDate.toTimeString() );
+    
+    // res.write(); // ok even if we call next
+    next();
+
+    console.log( 'response is being sent 2' );
+    console.log( 'New request received at time ', (new Date()).toTimeString() );
+
+    // if we send an end of response message, then no further middleware is called
+    // res.end( res.locals.receivedDate.toString() );
+});
+
+// req.body (JS object)
+app.use( express.json() );
+app.use( express.urlencoded( { extended: false } ) );
+
+app.use( express.static( path.join( process.cwd(), 'public' ) ) );
 app.use( indexRouter );
+
+// app.use( '/data', ( req, res ) => {
+//     res.json( req.body );
+// });
 
 // EXERCISE
 // sendFile( path.join( __dirname, ... ) ) on /contact
