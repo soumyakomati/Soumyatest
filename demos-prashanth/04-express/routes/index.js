ac8537b3ac837ed3ce107e1f4211b10ed6c73b02
Prashanth Puranik <puranik@digdeeper.in>
2021-07-09 22:05:56 +0530

    introduced data binding

--- demos-prashanth/04-express/routes/index.js
+++ demos-prashanth/04-express/routes/index.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 router.get( '/', ( req, res ) => {
     // res.send( 'Welcome to workshops app Home' );
-    res.render( 'index' );
+    res.render( 'index', {
+        name: 'Workshops App',
+        techStack: [
+            'Node',
+            'Express',
+            'EJS'
+        ]
+    });
 });
 
 module.exports = router;
