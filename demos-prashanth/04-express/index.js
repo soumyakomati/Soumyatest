7e0f1be130dd7eb1aca9372c0dcc6e0a7da54f6b
Prashanth Puranik <puranik@digdeeper.in>
2021-07-09 21:45:28 +0530

    enabled EJS

--- demos-prashanth/04-express/index.js
+++ demos-prashanth/04-express/index.js
@@ -1,6 +1,23 @@
 const express = require( 'express' );
+const indexRouter = require( './routes/' ); // by default the index file is imported
+const path = require( 'path' );
 
 const app = express();
+
+app.set( 'view engine', 'ejs' );
+app.set( 'views', path.join( __dirname, 'views' ) );
+
+app.use( indexRouter );
+
+// EXERCISE
+// sendFile( path.join( __dirname, ... ) ) on /contact
+
+// 404 error handler (if no other route matches this will and it serves an error response)
+// We do not specify the path here - so for every request this shall be matched (if none of the earlier ones matched)
+app.use( ( req, res ) => {
+    res.status( 404 );
+    res.send( 'Page not found' );
+});
 
 app.listen( 3000, ( err ) => {
     if( err ) {
